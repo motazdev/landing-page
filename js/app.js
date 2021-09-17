05969ee077cfee176fb1602f353900bbe49e70ef
motaz essam <motazessamdev@gmail.com>
2021-09-17 17:42:28 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -37,10 +37,13 @@
 // build the nav
 
 function navCreate() {
+
+    // adding new sections \\
+
     let sectionsLength = document.querySelectorAll('section').length;
     secId = sectionsLength + 1;
     let secCode = `
-    <section id="section${secId}" data-nav="Section ${secId}" class="">
+    <section id="section${secId}" class="">
     <div class="landing__container">
         <h2>Section ${secId}</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
@@ -49,18 +52,18 @@ function navCreate() {
     </div>
     </section>        
     `
+    // adding the new section before closing of "main" tag.
     document.querySelector('main').insertAdjacentHTML('beforeend', secCode);
 
     // adding navs with the same idea of adding new sections \\
-
-
     /*
     * i can get navs length again but its equal to
     * sections length and it already declared (and the same thing about 'section id' and 'nav item id')
     let navsLength = document.querySelectorAll('.menu__item').length;
     */
+
     let navItemCode = `
-    <li class="menu__item" data-secnum="${secId}"><a href="#section${secId}">Section ${secId}</a></li>
+    <li class="menu__item"><a href="#section${secId}">Section ${secId}</a></li>
     `
     document.querySelector('#l__content').insertAdjacentHTML('beforeend', navItemCode);
 
