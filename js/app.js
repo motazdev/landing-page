3cd6e10f22e52c484faa99bd063dc6d6a396c69f
motaz essam <53120830+motazdev@users.noreply.github.com>
2021-11-24 21:45:48 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -34,6 +34,33 @@
  * 
 */
 
+
+
+let sectionsLength = 4;
+for(let i=1; i<=sectionsLength; i++) {
+    let staticNavItemCode = `
+    <li class="menu__item"><a href="#section${i}">Section ${i}</a></li>
+    `    
+    document.querySelector('#l__content').insertAdjacentHTML('beforeend', staticNavItemCode);
+}
+
+for(let i=1; i<=sectionsLength; i++) {
+    let secCode = `
+    <section id="section${i}" class="">
+    <div class="landing__container">
+        <h2>Section ${i}</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    </div>
+    </section>        
+    `
+    // adding the new section before closing of "main" tag.
+    document.querySelector('main').insertAdjacentHTML('beforeend', secCode);
+}
+
+
+
 // build the nav
 
 function navCreate() {
